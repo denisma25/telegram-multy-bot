35a02c4b3ba04185f6a00da533241fdeef98bf75
denisma25 <66223203+denisma25@users.noreply.github.com>
2023-07-17 02:41:22 +0300

    added new modules. This is still Version 1.0

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from aiogram import Bot, Dispatcher
 from config import TOKEN
 
+import functions
+from data import my_dict
+from classes import *
+
+# полученный у @BotFather
+BOT_TOKEN: str = TOKEN
 
 
+# Создаем объекты бота и диспетчера
+bot: Bot = Bot(BOT_TOKEN)
+dp: Dispatcher = Dispatcher()
 
 
 
